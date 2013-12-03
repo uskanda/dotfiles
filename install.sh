0f10c418c93ece81e3ca1d3cd1a7889929cc5290
Yusuke Kanda <uskanda@gmail.com>
2013-12-03 20:06:40 +0900

    add bundler to installer

--- install.sh
+++ install.sh
@@ -5,4 +5,14 @@ if [[ ! -f ~/.nodebrew/nodebrew ]]; then
     echo Install NodeBrew...
     curl -L git.io/nodebrew | perl - setup
     echo done.
+else
+    echo NodeBrew exists. Skip install.
+fi
+
+# Bundler
+# RubyGems Manager
+if which bundle > /dev/null; then
+    echo Bundler exists. Skip install.
+else
+    gem install bundler
 fi
