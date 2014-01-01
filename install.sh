79c5045da91037f286be7f0ff4a4c365b2f7c89c
Yusuke Kanda <uskanda@gmail.com>
2014-01-02 00:25:09 +0900

    zsh-completionsから不要なものを削除

--- install.sh
+++ install.sh
@@ -20,3 +20,8 @@ fi
 # diff-highlight
 # Installed by git, make symbolic link to PATHed dir
 ln -s /usr/local/opt/git/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/
+
+for command in adb ag android brew bundle cap coffee git-flow jq knife middleman nvm node rails tmuxinator vagrant
+do
+  ln -s ~/.homesick/repos/unix-rc/home/.zsh/zsh-completions/src/_$command ~/.homesick/repos/unix-rc/home/.zsh/zsh-completions-selected/
+done
