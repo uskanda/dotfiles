d5515746015d9487cc2caacd316ff3548900e657
Yusuke Kanda <uskanda@gmail.com>
2014-02-16 09:58:41 +0900

    check xenv installed or not in install script

--- install.sh
+++ install.sh
@@ -1,18 +1,29 @@
-anyenv install rbenv
-anyenv install ndenv
-anyenv install pyenv
+
+for xenv in rbenv ndenv pyenv
+do
+  if [ `anyenv envs | grep -c $xenv` -eq 0 ]; then
+    echo "$xenv does not exist. start installing..."
+    anyenv install $xenv
+  else
+    echo "$xenv already exists. skip install."
+  fi
+done
 
 # Bundler
 # RubyGems Manager
 if which bundle > /dev/null; then
-    echo Bundler exists. Skip install.
+    echo bundler already exists. skip install.
 else
     gem install bundler
 fi
 
 # diff-highlight
 # Installed by git, make symbolic link to PATHed dir
-ln -s /usr/local/opt/git/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/
+if which diff-highlight > /dev/null; then
+  echo diff-highlight already exists. skip install.
+else
+  ln -s /usr/local/opt/git/share/git-core/contrib/diff-highlight/diff-highlight /usr/local/bin/
+fi
 
 for command in adb ag android brew bundle cap coffee git-flow jq knife middleman nvm node rails tmuxinator vagrant
 do
