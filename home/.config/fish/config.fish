bff023d4662351fe30386b42e5027c9a2a54c261
Yusuke Kanda <uskanda@gmail.com>
2018-04-04 15:52:07 +0900

    add fisherman install script

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -1,2 +1,11 @@
+# install fisherman if fisher command is missing
+if not type -q fisher
+  echo "fisherman does not exist. install now."
+  curl -s -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
+  fish -c "fisher"
+end
+
 set -U fish_prompt_pwd_dir_length 4
 source ~/.config/fish/config_local.fish
+
+#不服だがfunctions/completionsはfishermanにくれてやる
