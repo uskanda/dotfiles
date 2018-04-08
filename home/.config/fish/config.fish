ebcd0da8a8836667a9078c1776bc490535954a74
Yusuke Kanda <uskanda@gmail.com>
2018-04-08 16:18:41 +0900

    add bobthefish

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -10,4 +10,29 @@ source ~/.config/fish/config_local.fish
 
 set -U FZF_TMUX 1
 
+function do_enter
+  set -l query (commandline)
+
+  if test -n $query
+    echo
+    eval $query
+    commandline ''
+  else
+    echo
+    ls
+    if test (git rev-parse --is-inside-work-tree 2> /dev/null)
+      echo
+      echo -e "\e[0;33m--- git status ---\e[0m"
+     unbuffer git status -sb | head -n 1
+     unbuffer git status -s | git column
+     git --no-pager log -5 --oneline --decorate
+    end
+  end
+  commandline -f repaint
+end
+
+function fish_user_key_bindings
+  bind \cm do_enter
+end
+
 #不服だがfunctions/completionsはfishermanにくれてやる
--- home/.config/fish/config_local.fish
+++ home/.config/fish/config_local.fish
@@ -1,1 +1,6 @@
 # write down local settings
+set -g theme_display_git no
+set -g theme_display_git_dirty no
+set -g theme_display_git_untracked no
+set -g theme_project_dir_length 1
+
